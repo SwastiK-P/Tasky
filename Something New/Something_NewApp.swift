cb076d26e23d0c95ca7d8b2908302f38f9a7218c
Swastik Patil <swastikpatil000@gmail.com>
2025-02-09 10:27:20 +0530

    Initial Commit

---
+++ Something New/Something_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Something_NewApp.swift
+//  Something New
+//
+//  Created by Swastik Patil on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Something_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
